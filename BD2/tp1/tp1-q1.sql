646a1fb1af88c58cfc33ea78181f671d8821d686
Thomas Minier <tminier01@gmail.com>
2015-01-20 15:22:33 +0000

    suite tp1 - BD2

--- BD2/tp1/tp1-q1.sql
+++ BD2/tp1/tp1-q1.sql
@@ -1,2 +1,25 @@
 /* Requêtes effectuées sur la base */
 
+/* Question n°1 */
+SELECT *
+FROM Livres l
+WHERE (
+		SELECT COUNT(refl)
+		FROM Achats
+		WHERE refl = l.refl
+		) >= 10000;
+
+/* Question n°2 */
+SELECT *
+FROM Livres
+WHERE ((
+
+		)/(
+			SELECT COUNT(refl)
+			FROM Livres
+		))>= 16;
+
+/* Question n°3 */
+SELECT *
+FROM Clients NATURAL JOIN Avis
+WHERE commentaire = '';
