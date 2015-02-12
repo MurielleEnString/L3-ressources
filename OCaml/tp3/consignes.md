cdcfd48ec2d375017dad1d079051f10fa405ef30
Thomas Minier <tminier01@gmail.com>
2015-02-12 12:17:34 +0000

    ajout tp3 - OCaml

---
+++ OCaml/tp3/consignes.md
@@ -0,0 +1,19 @@
+Consignes du TP 3
+============
+
+Coder les fonctions suivantes :
+```
+dernier : 'a list -> 'a
+
+sans_dernier : 'a list -> 'a list (liste non vide)
+
+dernier_sans_dernier : 'a list -> 'a * 'a list (couple du dernier + liste sans le dernier)
+
+ajout_en_fin : 'a * 'a list -> 'a list
+
+inverse : 'a list -> 'a list (faire sans puis avec fonctions auxiliaires)
+
+colle : 'a list * 'a list -> 'a list (fusionne deux listes, équivaut à @ en OCaml)
+
+colle_toutes : 'a list list -> 'a list (faire avec colle puis sans utiliser de fonctions auxiliaires)
+```
---
+++ OCaml/tp3/tp3.ml
@@ -0,0 +1,49 @@
+(* Auteur : Thomas Minier, groupe 601A *)
+
+open List ;;
+
+(* Variables de test *)
+let x = [1;2;3;4;5;6;7;8;9];;
+let y = [[1;2;3];[4;5]] ;;
+
+let rec dernier lx =
+	if tl(lx) = []
+		then hd(lx)
+		else dernier(tl(lx)) ;;
+
+let rec sans_dernier lx =
+	if tl(lx) = []
+		then []
+		else hd(lx)::sans_dernier(tl(lx));; 
+
+let rec dernier_sans_dernier lx =
+	if tl(lx) = []
+		then (hd(lx), [])
+		else let (x,y) = dernier_sans_dernier(tl(lx))
+			in (x, hd(lx)::y) ;;
+
+let rec ajout_en_fin (x, lx) =
+	if lx = []
+		then [x]
+		else hd(lx)::ajout_en_fin(x, tl(lx));;
+
+let rec inverse lx =
+	if tl(lx) = []
+		then hd(lx)
+		else let 
+
+let rec colle (la, lb) =
+	if la = []
+		then if lb =  []
+			then []
+			else lb
+		else if lb = []
+			then la
+			else hd(la)::colle(tl(la), lb) ;;
+
+let rec colle_toutes lx =
+	if lx = []
+		then []
+		else if hd(lx) = []
+			then colle_toutes(tl(lx))
+			else hd(hd(lx))::colle_toutes(tl(hd(lx))::tl(lx)) ;;
