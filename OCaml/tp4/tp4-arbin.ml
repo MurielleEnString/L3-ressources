23420d42b4bec4198bda6370a0c8c9c73fbf239b
Thomas Minier <tminier01@gmail.com>
2015-03-05 12:03:44 +0000

    arbin <-> list fonctionnel - OCaml

--- OCaml/tp4/tp4-arbin.ml
+++ OCaml/tp4/tp4-arbin.ml
@@ -33,6 +33,13 @@ let z = embranche_bin((0,'a'),
 												arbin_vide())
 					);;
 
+let maxi (x,y) = max x y;;
+
+let rec pow (x,y) = 
+	if y = 0
+		then 1
+		else x * pow(x, y - 1) ;;
+
 let est_arbin_feuille a = 
 	est_arbin_vide(sag(a)) && est_arbin_vide(sad(a)) ;;
 
@@ -59,3 +66,49 @@ let rec de_meme_forme_que (a, b) =
 		else if est_arbin_vide(a) || est_arbin_vide(b)
 			then false (* cas de deux noeuds où l'un est là mais l'autre est absent*)
 			else de_meme_forme_que(sag(a), sag(b)) && de_meme_forme_que(sad(a), sad(b)) ;;
+
+(* fonctions auxiliaires pour list_of_arbin *)
+let rec entre (a,b) = 
+	if a > b
+		then arbin_vide()
+		else embranche_bin(a, entre(2*a, b), entre(2*a + 1, b)) ;;
+
+let rec rassemble_bin (a,b) =
+	if est_arbin_vide(a)
+		then arbin_vide()
+		else embranche_bin((rac(a), rac(b)), rassemble_bin(sag(a), sag(b)), rassemble_bin(sad(a), sad(b))) ;;
+
+let rec rgd a =
+	if est_arbin_vide(a)
+		then []
+		else [rac(a)]@rgd(sag(a))@rgd(sad(a)) ;;
+
+let rec hauteur_bin a =
+	if est_arbin_vide(a)
+		then 0
+		else 1 + maxi(hauteur_bin(sag(a)), hauteur_bin(sad(a))) ;;
+
+let list_of_arbin a =
+	let h = hauteur_bin(a)
+		in let etqs = entre(1, pow(2,h + 1) - 1)
+			in let arbre_couples = rassemble_bin(a, etqs)
+				in rgd(arbre_couples) ;;
+
+(* fonctions auxiliaires pour arbin_of_list *)
+
+(* donne le couple (la liste entre les indices x et y *, le reste de la liste) *)
+let rec sub_list (x, y, lx) = 
+	if lx = []
+		then ([], [])
+		else if x = y
+			then ( [], lx)
+			else let (a, b) = sub_list(x + 1, y, tl(lx))
+					in ([hd(lx)]@a, b) ;;
+ 
+let rec arbin_of_list lx =
+	if lx = []
+		then arbin_vide()
+		else let (racine, etq) = hd(lx)
+				in let (arbre_gauche, arbre_droit) = sub_list(0, 2 * etq + 1, tl(lx))
+					in embranche_bin(racine, arbin_of_list(arbre_gauche), arbin_of_list(arbre_droit)) ;;
+ 
